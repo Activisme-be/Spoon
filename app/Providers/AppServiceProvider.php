b20c30bddc9766bcecf4348caa31304d9ff0ca67
Tim Joosten <Tjoosten3@gmail.com>
2019-02-22 01:16:45 +0100

    Implementatie laravel/telescope
    Debugging tool voor tijdens het ontwikkelen aan de spoon  applicatie.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Telescope\TelescopeServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
