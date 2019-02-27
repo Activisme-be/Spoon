e2df5b53333267130975bd68ef63e595d09e7a8e
Tim Joosten <Tjoosten3@gmail.com>
2019-02-27 10:22:43 +0100

    Implementatie account beveiliging weergave

--- app/Http/Controllers/Users/AccountController.php
+++ app/Http/Controllers/Users/AccountController.php
@@ -37,7 +37,8 @@ class AccountController extends Controller
         // Determine on the switch statement which vierw the user wants to display
         // for modifying this account settings 
         switch ($request->type) {
-            default: return view('users.settings.information');
+            case 'beveiliging': return view('users.settings.security');
+            default:            return view('users.settings.information');
         }
     }
 
