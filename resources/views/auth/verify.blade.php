51735620d1388c43914b03158b8708f3fe673b6f
Tjoosten <Topairy@gmail.com>
2019-12-16 20:23:33 +0100

    Fix #108 Sync verify authentication stub.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,36 @@
-@extends('layouts.auth')
+@extends('layouts.auth', ['title' => 'Bevestig account'])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div class="container-fluid py-3">
+        <h1 class="page-title">Bevestig uw account</h1>
+    </div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
+    <div class="container-fluid pb-3">
+        <div class="card">
+            <div class="card-body">
+                @if (session('resent'))
+                    <div class="alert alert-success border-0" role="alert">
+                        Een nieuwe verificatie link is verzonden naar je email adres.
+                    </div>
+                @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
+                <p class="card-text">
+                    Uw account is momenteel nog niet bevestigd in {{ config('app.name') }}. Om deze applicaite volledig te kunnen gebruiken moet
+                    je je account bevestigen.
+                </p>
+
+                <p class="card-text">
+                    Voor dat u doorgaat met het bevestigen van je account. Vragen we je voor alle zekerheid je email adres nog is na te kijken. <br>
+                    Indien u geen mail ontvangt kunt u je spam folder nakijken. En of eventueel contact opnemen met de beheerder.
+                </p>
             </div>
+
+            <form class="card-footer bg-card-footer" method="POST" action="{{ route('verification.resend') }}">
+                @csrf
+                <button type="submit" class="btn btn-secondary">
+                    <i class="fe fe-send"></i> Account bevestigen
+                </button>
+            </form>
         </div>
     </div>
-</div>
 @endsection
