8b2808fc9517a656be93668ca102ab0172ba7f78
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-12-20 07:35:43 +0100

    Fix #106 Implementatie core config (#114)

---
+++ config/core.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+     | --------------------------------------------------------------------------
+     | Core Configuration File
+     | --------------------------------------------------------------------------
+     |
+     | Here you may specify the configuration values that are specific for the
+     | application that u building.
+     |
+     */
+
+];
