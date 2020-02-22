247731efc3989e76651938e1de149f27d09b4269
Tim Joosten <Tjoosten3@gmail.com>
2020-02-22 04:29:36 +0100

    Fix #126 Data pruning laravel telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,10 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
         $schedule->command('backup:monitor')->twiceDaily('02:30', );
+
+        if (app()->isLocal()) {
+            $schedule->command('telescope:prune')->daily();
+        }
     }
 
     /**
