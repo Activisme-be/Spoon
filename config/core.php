766645fde19cbc9e9897432d8aaf297e939b6d6e
Tjoosten <Tjoosten3@gmail.com>
2020-07-27 06:00:00 +0200

    Remove the old seeding system

--- config/core.php
+++ config/core.php
@@ -3,17 +3,6 @@
 return [
 
     /**
-     * The default users trough the seeds. only the First and lastname is required because the other data is filled up
-     * with the default pass that is 'pasword'. And your email is in the following format.
-     *
-     * <firstname>@<application mail domain>
-     */
-
-    'users' => [
-        ['Tim', 'Joosten'], ['Sara', 'Landuyt'], ['Tom', 'Manheaghe']
-    ],
-
-    /**
      * The 2FA config variable where u currently one can define if you want to use the 2FA authentication or not.
      */
 
