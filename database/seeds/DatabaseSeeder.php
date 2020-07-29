6674f69b0957ee5206354aec7560da45d02b2a72
Tjoosten <Tjoosten3@gmail.com>
2020-07-29 09:39:59 +0200

    Revert "Remove the old seeding system"
    This reverts commit 766645fd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,31 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::connection()->disableQueryLog();
+        Model::unguard();
 
+        $this->truncateAll();
+
+        // Run database seeders
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+    }
+
+    protected function truncateAll(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        collect(DB::select("SHOW FULL TABLES WHERE Table_Type = 'BASE TABLE'"))
+            ->map(static function (stdClass $tableProperties): string {
+                return get_object_vars($tableProperties)[key($tableProperties)];
+            })
+            ->reject(static function (string $tableName): bool {
+                return $tableName === 'migrations';
+            })
+            ->each(static function (string $tableName): void {
+                DB::table($tableName)->truncate();
+            });
+
+        Schema::enableForeignKeyConstraints();
     }
 }
